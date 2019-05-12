8c539946355eb248db74a43c142c81755dae4f5a
Chloé "Matcha" Desoutter <chloe.desoutter@gmail.com>
2019-05-12 18:49:05 +0200

    Added utils and tweaked i3

--- .vimrc
+++ .vimrc
@@ -1,6 +1,16 @@
 execute pathogen#infect()
-
 syntax on
+
+" compliance with python styling
+set tabstop=4 
+set shiftwidth=4
+set expandtab
 set background=dark
+
+" don't mess with Makefiles
+autocmd FileType make setlocal noexpandtab
+
+" aesthetics
 colorscheme solarized
+
 
---
+++ bin/spectro-analysis.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# a script for audiophiles/use this to analyze spectra of your audio
+SONG=$1
+START_TIME=$2
+DURATION=${3:-0:02}
+
+BN=$(basename "$SONG" .flac)
+if [ "" = "$START_TIME" ];then
+sox "${SONG}" -n remix 1 spectrogram -x 3000 -y 513 -z 120 -w Kaiser -o "$HOME/spectro-analysis/${BN}.spectre.png"
+else
+sox "${SONG}" -n remix 1 spectrogram -x 3000 -y 513 -z 120 -w Kaiser -S ${START_TIME} -d ${DURATION} -o "$HOME/spectro-analysis/${BN}.spectre.zoom.${START_TIME}__${DURATION}.png"
+fi
